57506cb453f750cfbfff7bfee0ca8dfef83c3960
sourabhpal <nick.tdr19@gmail.com>
2025-09-01 18:56:37 +0530

    initial commit

---
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,25 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import sourabh.pal.mysocial.configureKotlinAndroid
+
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.application")
+            apply(plugin = "org.jetbrains.kotlin.android")
+            //apply(plugin = "mysocial.android.lint")
+            //apply(plugin = "com.dropbox.dependency-guard")
+
+            extensions.configure<ApplicationExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 36
+                @Suppress("UnstableApiUsage")
+                testOptions.animationsDisabled = true
+            }
+        }
+    }
+}
---
+++ build-logic/convention/src/main/java/sourabh/pal/mysocial/KotlinAndroid.kt
@@ -0,0 +1,50 @@
+package sourabh.pal.mysocial
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+
+/**
+ * Configure base Kotlin with Android options
+ */
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 35
+
+        defaultConfig {
+            minSdk = 21
+        }
+
+        compileOptions {
+            // Up to Java 11 APIs are available through desugaring
+            // https://developer.android.com/studio/write/java11-minimal-support-table
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
+    extensions.configure<KotlinAndroidProjectExtension> {
+        jvmToolchain(11)
+    }
+}
+
+/**
+ * Configure base Kotlin options for JVM (non-Android)
+ */
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        // Up to Java 11 APIs are available through desugaring
+        // https://developer.android.com/studio/write/java11-minimal-support-table
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    extensions.configure<KotlinAndroidProjectExtension> {
+        jvmToolchain(11)
+    }
+}
